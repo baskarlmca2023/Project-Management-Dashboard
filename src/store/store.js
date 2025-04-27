a038da29413780b5654c10d5f55fc48a8eb28ba6
Baskar L <139743997+baskarlmca2023@users.noreply.github.com>
2025-04-27 13:21:50 +0530

    Update store.js

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,15 @@
-
 import { configureStore } from '@reduxjs/toolkit';
 import employeeReducer from '../features/employees/employeeSlice';
 import projectReducer from '../features/projects/projectSlice';
 import taskReducer from '../features/tasks/taskSlice';
+import authReducer from '../auth/authSlice'; 
 
 const store = configureStore({
   reducer: {
     employees: employeeReducer,
     projects: projectReducer,
     tasks: taskReducer,
+    auth: authReducer, 
   },
 });
 
